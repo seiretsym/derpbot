10af240d41f94f264aa2f7bba5033f70025c411e
Kerwin Hy <kerwin.hy@gmail.com>
2020-09-03 02:46:47 -0700

    add logic to disconnect bot before attempting to start it up again

--- bot/test.js
+++
@@ -1,2 +0,0 @@
-console.log("%F0%9F%99%83")
-console.log(encodeURIComponent("%F0%9F%99%83"))
--- server.js
+++ server.js
@@ -106,6 +106,7 @@ app.use(session({ secret: "derp", resave: true, saveUninitialized: true }));
 // routing
 // bot routes
 app.get("/api/bot/start", (req, res) => {
+  client.destroy();
   db.Channel.find({}).then(data => {
     if (data.length > 0) {
       // start the damn bot
